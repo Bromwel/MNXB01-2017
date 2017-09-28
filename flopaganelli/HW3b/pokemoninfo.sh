0cb4d1425d8e77bbd2e614cdfd5fb2988940a341
Bromwel Apondi <rh0260ap-s@student.lu.se>
2017-09-28 10:24:48 +0200

    This is HW3b initial.

--- flopaganelli/HW3b/pokemoninfo.sh
+++ flopaganelli/HW3b/pokemoninfo.sh
@@ -4,7 +4,7 @@
 #
 # MNXB01-2017-HW3b
 # File: pokemoninfo.sh.skeleton file
-# Author: Florido Paganelli florido.paganelli@hep.lu.se
+# Author: Bromwel Apondi rh0260ap-s@student.lu.se
 #         Lund University
 #
 ########################################################################
@@ -31,12 +31,25 @@ errormsg() {
 # hint: use the if construct and the proper conditions to verify the arguments
 
 # YOUR CODE HERE
-
+if [ ! $DBDIR ]; then
+	echo "You need to have at least one argument!" ;
+	# Exit with error, not zero
+	exit 1;
+fi
 ### Exercise 2: 1 points
 # Write an error and exit if the DBDIR directory does not exist or it's not a directory.
 # Hint: read http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
 
 # YOUR CODE HERE
+echo "This scripts checks the existance of the DBDIR directory."
+echo "Checking..."
+if [ ! -d $DBDIR ]
+	then
+		echo "$DBDIR does not  exist or is not a directory!"
+		# Exit with error, zero
+		exit 0;
+	
+fi
 
 ### Exercise 3: 1 point
 # Use the grep command to find which file contains "Pokémon Red Version"
@@ -44,12 +57,16 @@ errormsg() {
 # grep examples: http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_04_02.html
 
 echo -e "\nSearching for Pokémon Red..."
-# YOUR CODE HERE
+
+grep -nr --include=*.csv "Pokémon Red Version" $DBDIR
 
 ### Exercise 4: 1 point
 # delete existing allplatform.csv file in preparation of the next exercise
 echo -e "\nRemoving old allplatforms.csv"
 # YOUR CODE HERE
+if [ -e ./allplatforms.csv ]; 
+	then rm ./allplatforms.csv 
+fi
 
 ### Exercise 5: 3 points
 # Write a for loop that takes every file in the database and puts it 
@@ -65,18 +82,24 @@ echo -e "\nRemoving old allplatforms.csv"
 echo -e "\nCreating new allplatforms.csv"
 
 # YOUR FOR LOOP HERE
+Outfiles="allplatforms.csv"
 
+for files in $DBDIR/*.csv ; do
+	tail -n +2 $files >> $Outfiles
+done	
 
-### Exercise 4: 1 point
+### Exercise 6: 1 point
 # Sort the contents of the allplatforms.csv file by using the sort 
 # command and write the result in allplatforms.ordered.csv
 # Hint: use \" as a delimiter for sort. Check 'man sort'
 echo -e "\nSorting allplatforms.csv..."
 # YOUR CODE HERE
 
+Alfabetical="allplatforms.ordered.csv"
 
+sort -k2 -d ./allplatforms.csv >> $Alfabetical
 
-# Exercise 5: 4 points
+# Exercise 7: 4 points
 # Write a for loop that, for each file, counts all the games
 # in each file. Inspect the csv file to understand the structure of the 
 # csv file to get the right numbers.
@@ -94,5 +117,12 @@ echo -e "\nSorting allplatforms.csv..."
 echo -e "\nCalculating number of games for each file..."
 
 #YOUR CODE HERE
+
+for files in $DBDIR/*.csv ; do
+	gamenum="$(tail -n +2 $files | wc -l)"
+	filename=$(basename "$files")
+	echo "$filename has $gamenum games"
+	
+done
 
 exit 0;
--- flopaganelli/Tutorial3bMaterial/bash/getcpuinfo.sh
+++ flopaganelli/Tutorial3bMaterial/bash/getcpuinfo.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # put the output of cat in the variable CPUINFO
 CPUINFO=$(cat /proc/cpuinfo | head -10)
 
 # write the content of CPUINFO to screen
 echo "$CPUINFO"
+
+echo "PATH value is $PATH"
